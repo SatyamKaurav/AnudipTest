bb7a38ef6e703024a9f3e1a85258178daf29ce20
SATYAM KAURAV <93872311+SatyamKaurav@users.noreply.github.com>
2024-10-23 21:17:29 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,20 +5,30 @@ public class Main {
 
         //Question :  Find Largest Element in an Array: Write a program to find the largest element in a given integer array.
 
-        int[] arr = {1,45,88,45,78,12,20};
+        int[] arr = {45,88,45,78,12,20};
         System.out.println(largestElementInArray(arr));
     }
 
+
+    //I am  creating this method to find the largest element in a given array 
     private static int largestElementInArray(int[] arr) {
 
+        //This variable will store the length of the array
         int length = arr.length;
 
-        int max = arr[0];
+        //here I am creating a variable max which initially store the Min value
+        int max = Integer.MIN_VALUE;
+
         // here we will iterate over the array one by one
         for (int i =0 ; i < length-1 ; i++){
+
+
+          //Here we are iterating over the array and comparing each element of the array 
             if(arr[i] > max)
                 max = arr[i];
+
         }
+
         return max;
     }
 }
